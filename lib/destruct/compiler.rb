b5da61a651ed2df103becb9aa8e67b09b5e184d3
Peter Winton <pwinton@indigobio.com>
2018-09-02 17:00:20 -0400

    compiling vars

--- lib/destruct/compiler.rb
+++ lib/destruct/compiler.rb
@@ -11,22 +11,29 @@ class Destruct
       end
     end
 
+    def initialize
+      @refs = {}
+    end
+
     def compile(pat)
+      matching_code = emit(pat, "x")
       code = <<~CODE
-        proc do
-          proc do |x, binding, env|
-            #{emit(pat, "x")}
+        lambda do |#{@refs.keys.join(", ")}|
+          lambda do |x, binding, env|
+            #{matching_code}
+            env ||= ::Destruct::Env.new
           end
         end
       CODE
       code = beautify_ruby(code)
       puts number_lines(code)
-      compiled = eval(code).call
+      compiled = eval(code).call(*@refs.values)
       CompiledPattern.new(pat, compiled)
     end
 
     def emit(pat, x_expr)
       if pat.is_a?(Var)
+        emit_var(pat, x_expr)
       else
         emit_literal(pat, x_expr)
       end
@@ -34,12 +41,21 @@ class Destruct
 
     def emit_literal(pat, x_expr)
       <<~CODE
-        if #{x_expr} == #{pat.inspect}
-          env ||= ::Destruct::Env.new
-        else
-          nil
-        end
+        return nil unless #{x_expr} == #{pat.inspect}
       CODE
+    end
+
+    def emit_var(pat, x_expr)
+      <<~CODE
+        env ||= ::Destruct::Env.new
+        return nil unless env.bind(#{get_ref(pat)}, #{x_expr})
+      CODE
+    end
+
+    def get_ref(pat)
+      id = "_ref#{@refs.size}"
+      @refs[id] = pat
+      id
     end
 
     def beautify_ruby(code)
--- spec/lib/destruct/compiler_spec.rb
+++ spec/lib/destruct/compiler_spec.rb
@@ -2,7 +2,7 @@ require 'destruct'
 
 class Destruct
   describe Compiler do
-    it 'compile literals' do
+    it 'compiles literals' do
       cp = Compiler.compile(1)
       expect(cp.match(1)).to be_a Env
       expect(cp.match(2)).to be_nil
@@ -10,6 +10,12 @@ class Destruct
       cp = Compiler.compile("foo")
       expect(cp.match("foo")).to be_a Env
       expect(cp.match("bar")).to be_nil
+    end
+    it 'compiles vars' do
+      cp = Compiler.compile(Var.new(:foo))
+      e = cp.match(1)
+      expect(e).to be_an Env
+      expect(e[:foo]).to eql 1
     end
   end
 end
