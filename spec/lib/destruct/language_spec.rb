84a70b77fb352392b4409173d71ed22471a40d8d
Peter Winton <pwinton@indigobio.com>
2018-09-02 23:14:03 -0400

    wip

--- spec/lib/destruct/language_spec.rb
+++ spec/lib/destruct/language_spec.rb
@@ -7,12 +7,12 @@ class Destruct
     it 'built-in rules' do
       lang = Language.new
       expect(lang.translate { 1 }).to eql 1
-      # expect(lang.translate { 2.0 }).to eql 2.0
-      # expect(lang.translate { :x }).to eql :x
-      # expect(lang.translate { 'x' }).to eql 'x'
-      # x_var = lang.translate { x }
-      # expect(x_var).to be_a Var
-      # expect(x_var.name).to eql :x
+      expect(lang.translate { 2.0 }).to eql 2.0
+      expect(lang.translate { :x }).to eql :x
+      expect(lang.translate { 'x' }).to eql 'x'
+      x_var = lang.translate { x }
+      expect(x_var).to be_a Var
+      expect(x_var.name).to eql :x
     end
     it 'passes matches to the block' do
       lang = Language.new
