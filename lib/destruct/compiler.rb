7460d8c0ce3f62c0cd342baac48a0e6c3536d269
Peter Winton <pwinton@indigobio.com>
2019-05-16 14:06:10 -0400

    wip

--- lib/destruct/compiler.rb
+++ lib/destruct/compiler.rb
@@ -62,7 +62,7 @@ class Destruct
 
     module HasMeta
       def possible_values
-        @possible_values ||= [] #[false, nil, :truthy]
+        @possible_values ||= []
       end
 
       def possible_values=(x)
@@ -250,12 +250,12 @@ class Destruct
           else
             var, *vars = params
             val, *vals = args
-            _let(var, val, normalize(_apply(_lambda(vars, body), *vals)))
+            _let(var, normalize(val), normalize(_apply(_lambda(vars, body), *vals)))
           end
-        elsif match { form(:if, cond, cons, alt) }
-          tval = ident?(cond) ? cond : ident
-          _let(tval, normalize(cond),
-               _if(tval, normalize(cons), normalize(alt)))
+        elsif match { form(:if, cond, cons, alt) } && !ident?(cond)
+          t = ident
+          _let(t, normalize(cond),
+               _if(t, normalize(cons), normalize(alt)))
         else
           tx(x, :normalize)
         end
@@ -346,7 +346,7 @@ class Destruct
       elsif x.is_a?(HasMeta)
         x.possible_values
       else
-        [] #[false, nil, :truthy]
+        []
       end
     end
 
@@ -617,61 +617,6 @@ class Destruct
       contains_forms?(%i(if let begin), x)
     end
 
-    # inline stuff
-    def inline_stuff(x)
-      counts = Hash.new { |h, k| h[k] = 0 }
-      map = {}
-      count_refs!(x, counts, map)
-      map.delete_if { |k, _| counts[k] > 1 }
-      inline(x, map)
-    end
-
-    # def inline(x, map)
-    #   map_form(x) do |recurse|
-    #     destruct(x) do
-    #       if match { form(:set!, lhs, _) } && map.keys.include?(lhs)
-    #         _noop
-    #       elsif match { form(:ident, _) }
-    #         map.fetch(x, x)
-    #       else
-    #         recurse.call { |c| inline(c, map) }
-    #       end
-    #     end
-    #   end
-    # end
-
-    # def count_refs!(x, counts, map)
-    #   map_form(x) do |recurse|
-    #     destruct(x) do
-    #       if match { form(:set!, lhs, rhs) }
-    #         map[lhs] = rhs
-    #         count_refs!(rhs, counts, map)
-    #       elsif match { form(:ident, _) }
-    #         counts[x] += 1
-    #       else
-    #         recurse.call { |c| count_refs!(c, counts, map) }
-    #       end
-    #     end
-    #   end
-    #   nil
-    # end
-
-    def squash_begins(x)
-      if x.type == :begin
-        squashed_children =
-            x.children
-                .map { |c| squash_begins(c) }
-                .reject { |c| c.is_a?(Form) && c.type == :begin && c.children.none? }
-        if squashed_children.size == 1
-          squashed_children.first
-        else
-          _begin(*squashed_children)
-        end
-      else
-        tx(x, :squash_begins)
-      end
-    end
-
     def literal_val?(x)
       [TrueClass, FalseClass, NilClass, Numeric, String, Symbol, Module].any? { |c| x.is_a?(c) }
     end
@@ -703,22 +648,6 @@ class Destruct
     def _set!(var, val)
       Form.new(:set!, var, val)
     end
-
-    # def remove_bind(x)
-    #   destruct(x) do
-    #     if match { form(:bind, env, sym, val) }
-    #       if ident?(env)
-    #         _begin(_set_field(env, sym, val), env)
-    #       else
-    #         t = ident("env")
-    #         _let(t, remove_bind(env),
-    #              _begin(_set_field(t, sym, val), t))
-    #       end
-    #     else
-    #       tx(x, :remove_bind)
-    #     end
-    #   end
-    # end
 
     def emit_ruby(x)
       destruct(x) do
@@ -780,14 +709,6 @@ class Destruct
         "(#{emit_ruby(x)})"
       else
         emit_ruby(x)
-      end
-    end
-
-    def multival(ss)
-      if ss.size == 1
-        ss.first
-      else
-        "(#{ss.join("; ")})"
       end
     end
 
@@ -1048,37 +969,6 @@ class Destruct
       end
     end
 
-    def match(s)
-      if s.pat == Any
-        # do nothing
-      elsif s.pat.is_a?(Obj)
-        match_obj(s)
-      elsif s.pat.is_a?(Or)
-        match_or(s)
-      elsif s.pat.is_a?(Let)
-        match_let(s)
-      elsif s.pat.is_a?(Var)
-        match_var(s)
-      elsif s.pat.is_a?(Unquote)
-        match_unquote(s)
-      elsif s.pat.is_a?(Hash)
-        match_hash(s)
-      elsif s.pat.is_a?(Array)
-        match_array(s)
-      elsif s.pat.is_a?(Regexp)
-        match_regexp(s)
-      elsif s.pat.is_a?(Strict)
-        match_strict(s)
-      elsif is_literal_val?(s.pat)
-        match_literal(s)
-      elsif match_other(s)
-      end
-    end
-
-    # def is_literal_val?(x)
-    #   x.is_a?(Numeric) || x.is_a?(String) || x.is_a?(Symbol)
-    # end
-
     def is_literal_pat?(p)
       !(p.is_a?(Obj) ||
           p.is_a?(Or) ||
@@ -1101,308 +991,6 @@ class Destruct
       else
         1
       end
-    end
-
-    def match_array(s)
-      s.type = :array
-      splat_count = s.pat.count { |p| p.is_a?(Splat) }
-      if splat_count > 1
-        raise "An array pattern cannot have more than one splat: #{s.pat}"
-      end
-      splat_index = s.pat.find_index { |p| p.is_a?(Splat) }
-      is_closed = !splat_index || splat_index != s.pat.size - 1
-      pre_splat_range = 0...(splat_index || s.pat.size)
-
-      s.x = localize(nil, s.x)
-      known_real_envs_before = @known_real_envs.dup
-      emit_if "#{s.x}.is_a?(Array)" do
-        cond = splat_index ? "#{s.x}.size >= #{s.pat.size - 1}" : "#{s.x}.size == #{s.pat.size}"
-        test(s, cond) do
-
-          pre_splat_range
-              .map { |i| [s.pat[i], i] }
-              .sort_by { |(item_pat, i)| [pattern_order(item_pat), i] }
-              .each do |item_pat, i|
-            x = localize(item_pat, "#{s.x}[#{i}]")
-            match(Frame.new(item_pat, x, s.env, s))
-          end
-
-          if splat_index
-            splat_range = get_temp("splat_range")
-            post_splat_width = s.pat.size - splat_index - 1
-            emit "#{splat_range} = #{splat_index}...(#{s.x}.size#{post_splat_width > 0 ? "- #{post_splat_width}" : ""})"
-            bind(s, s.pat[splat_index], "#{s.x}[#{splat_range}]")
-
-            post_splat_pat_range = ((splat_index + 1)...s.pat.size)
-            post_splat_pat_range.each do |i|
-              item_pat = s.pat[i]
-              x = localize(item_pat, "#{s.x}[-#{s.pat.size - i}]")
-              match(Frame.new(item_pat, x, s.env, s))
-            end
-          end
-        end
-      end.elsif "#{s.x}.is_a?(Enumerable)" do
-        @known_real_envs = known_real_envs_before
-        en = get_temp("en")
-        done = get_temp("done")
-        stopped = get_temp("stopped")
-        emit "#{en} = #{s.x}.each"
-        emit "#{done} = false"
-        emit_begin do
-          s.pat[0...(splat_index || s.pat.size)].each do |item_pat|
-            x = localize(item_pat, "#{en}.next")
-            match(Frame.new(item_pat, x, s.env, s))
-          end
-
-          if splat_index
-            if is_closed
-              splat = get_temp("splat")
-              emit "#{splat} = []"
-              splat_len = get_temp("splat_len")
-              emit "#{splat_len} = #{s.x}.size - #{s.pat.size - 1}"
-              emit "#{splat_len}.times do"
-              emit "#{splat} << #{en}.next"
-              emit "end"
-              bind(s, s.pat[splat_index], splat)
-
-              s.pat[(splat_index + 1)...(s.pat.size)].each do |item_pat|
-                x = localize(item_pat, "#{en}.next")
-                match(Frame.new(item_pat, x, s.env, s))
-              end
-            else
-              bind(s, s.pat[splat_index], "#{en}.new_from_here")
-            end
-          end
-
-          emit "#{done} = true"
-          emit "#{en}.next" if is_closed
-        end.rescue "StopIteration" do
-          emit "#{stopped} = true"
-          test(s, done)
-        end.end
-        test(s, stopped) if is_closed
-      end.else do
-        test(s, "nil")
-      end
-    end
-
-    def in_or(s)
-      !s.nil? && (s.type == :or || in_or(s.parent))
-    end
-
-    def in_strict(s)
-      !s.nil? && (s.pat.is_a?(Strict) || in_strict(s.parent))
-    end
-
-    def match_regexp(s)
-      s.type = :regexp
-      m = get_temp("m")
-      match_env = get_temp("env")
-      test(s, "#{s.x}.is_a?(String) || #{s.x}.is_a?(Symbol)") do
-        emit "#{m} = #{get_ref(s.pat)}.match(#{s.x})"
-        emit "#{match_env} = Destruct::Env.new(#{m}) if #{m}"
-        test(s, match_env)
-        merge(s, match_env, dynamic: true)
-      end
-    end
-
-    def match_strict(s)
-      match(Frame.new(s.pat.pat, s.x, s.env, s))
-    end
-
-    def match_literal(s)
-      s.type = :literal
-      test(s, "#{s.x} == #{s.pat.inspect}")
-    end
-
-    def match_other(s)
-      s.type = :other
-      test(s, "#{s.x} == #{get_ref(s.pat)}")
-    end
-
-    # def test(s, cond)
-    #   # emit "puts \"line #{emitted_line_count + 8}: \#{#{cond.inspect}}\""
-    #   emit "puts \"test: \#{#{cond.inspect}}\"" if $show_tests
-    #   if in_or(s)
-    #     emit "#{s.env} = (#{cond}) ? #{s.env} : nil if #{s.env}"
-    #     if block_given?
-    #       emit_if s.env do
-    #         yield
-    #       end.end
-    #     end
-    #   elsif cond == "nil" || cond == "false"
-    #     emit "return nil"
-    #   else
-    #     emit "#{cond} or return nil"
-    #     yield if block_given?
-    #   end
-    # end
-
-    def match_var(s)
-      s.type = :var
-      test(s, "#{s.x} != #{nothing_ref}")
-      bind(s, s.pat, s.x)
-    end
-
-    def match_unquote(s)
-      temp_env = get_temp("env")
-      emit "raise 'binding must be provided' if _binding.nil?"
-      emit "#{temp_env} = Destruct.match((_binding.respond_to?(:call) ? _binding.call : _binding).eval('#{s.pat.code_expr}'), #{s.x}, _binding)"
-      test(s, temp_env)
-      merge(s, temp_env, dynamic: true)
-    end
-
-    def match_let(s)
-      s.type = :let
-      match(Frame.new(s.pat.pattern, s.x, s.env, s))
-      bind(s, s.pat, s.x)
-    end
-
-    def bind_old(s, var, val, val_could_be_unbound_sentinel = false)
-      var_name = var.is_a?(Binder) ? var.name : var
-
-      # emit "# bind #{var_name}"
-      proposed_val =
-          if val_could_be_unbound_sentinel
-            # we'll want this in a local because the additional `if` clause below will need the value a second time.
-            pv = get_temp("proposed_val")
-            emit "#{pv} = #{val}"
-            pv
-          else
-            val
-          end
-
-      do_it = proc do
-        unless @known_real_envs.include?(s.env)
-          # no need to ensure the env is real (i.e., an Env, not `true`) if it's already been ensured
-          emit "#{s.env} = _make_env.() if #{s.env} == true"
-          @known_real_envs.add(s.env) unless in_or(s)
-        end
-        current_val = "#{s.env}.#{var_name}"
-        if @var_counts[var_name] > 1
-          # if the pattern binds the var in two places, we'll have to check if it's already bound
-          emit_if "#{current_val} == :__unbound__" do
-            emit "#{s.env}.#{var_name} = #{proposed_val}"
-          end.elsif "#{current_val} != #{proposed_val}" do
-            if in_or(s)
-              emit "#{s.env} = nil"
-            else
-              test(s, "nil")
-            end
-          end.end
-        else
-          # otherwise, this is the only place we'll attempt to bind this var, so just do it
-          emit "#{current_val} = #{proposed_val}"
-        end
-      end
-
-      if in_or(s)
-        emit_if("#{s.env}", &do_it).end
-      elsif val_could_be_unbound_sentinel
-        emit_if("#{s.env} && #{proposed_val} != :__unbound__", &do_it).end
-      else
-        do_it.()
-      end
-
-      test(s, "#{s.env}") if in_or(s)
-    end
-
-    def match_obj(s)
-      s.type = :obj
-      match_hash_or_obj(s, get_ref(s.pat.type), s.pat.fields, proc { |field_name| "#{s.x}.#{field_name}" })
-    end
-
-    def match_hash(s)
-      s.type = :hash
-      match_hash_or_obj(s, "Hash", s.pat, proc { |field_name| "#{s.x}.fetch(#{field_name.inspect}, #{nothing_ref})" },
-                        "#{s.x}.keys.sort == #{get_ref(s.pat.keys.sort)}")
-    end
-
-    def nothing_ref
-      get_ref(Destruct::NOTHING)
-    end
-
-    def match_hash_or_obj(s, type_str, pairs, make_x_sub, strict_test = nil)
-      test(s, "#{s.x}.is_a?(#{type_str})") do
-        keep_matching = proc do
-          pairs
-              .sort_by { |(_, field_pat)| pattern_order(field_pat) }
-              .each do |field_name, field_pat|
-            x = localize(field_pat, make_x_sub.(field_name), field_name)
-            match(Frame.new(field_pat, x, s.env, s))
-          end
-        end
-
-        if in_strict(s) && strict_test
-          test(s, strict_test) { keep_matching.call }
-        else
-          keep_matching.call
-        end
-      end
-    end
-
-    def multi?(pat)
-      pat.is_a?(Or) ||
-          (pat.is_a?(Array) && pat.size > 1) ||
-          pat.is_a?(Obj) && pat.fields.any?
-    end
-
-    def match_or(s)
-      s.type = :or
-      closers = []
-      or_env = get_temp("env")
-      emit "#{or_env} = true"
-      s.pat.patterns.each_with_index do |alt, i|
-        match(Frame.new(alt, s.x, or_env, s))
-        if i < s.pat.patterns.size - 1
-          emit "unless #{or_env}"
-          closers << proc { emit "end" }
-          emit "#{or_env} = true"
-        end
-      end
-      closers.each(&:call)
-      merge(s, or_env)
-      emit "#{s.env} or return nil" if !in_or(s.parent)
-    end
-
-    def merge(s, other_env, dynamic: false)
-      @known_real_envs.include?(s.env)
-
-      emit_if("#{s.env}.nil? || #{other_env}.nil?") do
-        emit "#{s.env} = nil"
-      end.elsif("#{s.env} == true") do
-        emit "#{s.env} = #{other_env}"
-      end.elsif("#{other_env} != true") do
-        if dynamic
-          emit "#{other_env}.env_each do |k, v|"
-          emit_if("#{s.env}[k] == :__unbound__") do
-            emit "#{s.env}[k] = v"
-          end.elsif("#{s.env}[k] != v") do
-            if in_or(s)
-              emit "#{s.env} = nil"
-            else
-              test(s, "nil")
-            end
-          end.end
-          emit "end"
-        else
-          @var_names.each do |var_name|
-            bind(s, var_name, "#{other_env}.#{var_name}", true)
-          end
-        end
-      end.end
-    end
-
-    private
-
-    def localize(pat, x, prefix = "t")
-      prefix = prefix.to_s.gsub(/[^\w\d_]/, '')
-      if (pat.nil? && x =~ /\.\[\]/) || multi?(pat) || (pat.is_a?(Binder) && x =~ /\.fetch|\.next/)
-        t = get_temp(prefix)
-        emit "#{t} = #{x}"
-        x = t
-      end
-      x
     end
   end
 
